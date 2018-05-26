9cdde7cbbace46fd25b5dff0395e4a80305a8d3c
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 05:09:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  result=Array.new
+  arraySize=array.size
+  case arraySize
+  when 1
+    return array.join
+    when 2
+      result=array.join(' and ')
+      return result
+    else
+      array[-1]="and "<<array[-1]
+      result = array.join(', ')
+      return result
+  end
 end
