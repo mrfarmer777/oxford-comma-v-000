7cfc1d6d8ce3ddf48ee92583f74dc09bfc379fbf
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 05:11:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   result=Array.new
   arraySize=array.size
   case arraySize
-  when 1
-    return array.join
+    when 1
+      return array.join
     when 2
       result=array.join(' and ')
       return result
